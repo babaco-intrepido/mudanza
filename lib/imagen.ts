4c83cdb0daed51e1940db10829d88f54af61feb6
Federico Aloi <federico.aloi@gmail.com>
2022-04-20 20:36:30 -0300

    Sanitizada URL de imágenes

--- lib/imagen.ts
+++ lib/imagen.ts
@@ -4,8 +4,14 @@ import { isEmpty } from 'ramda';
 
 const placeholder = 'placeholder_oz5mpd.png';
 
+// TODO: no debería ser necesario pero el CMS le está agregando una / al principio.
+function sanitizar(nombre: string) {
+  return nombre.replace('/', '');
+}
+
+// TODO: mover el cloudName a variable de entorno.
 export function imagenConResolucion(nombre: string, resize: Resize) {
-  return buildUrl(isEmpty(nombre) ? placeholder : nombre, {
+  return buildUrl(isEmpty(nombre) ? placeholder : sanitizar(nombre), {
     cloud: { cloudName: 'faloi' },
     transformations: { resize },
   });
