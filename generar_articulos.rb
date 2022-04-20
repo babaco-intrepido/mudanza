2eac34c878ef8c6b972b3f9b014e8c18e86f2ed6
Federico Aloi <federico.aloi@gmail.com>
2022-04-20 19:49:38 -0300

    El script funciona directamente con CSV

--- generar_articulos.rb
+++ generar_articulos.rb
@@ -1,23 +1,24 @@
 #!/usr/bin/env ruby
 
+require 'csv'
 require 'yaml'
 
 BASE_DIR = 'content/articulos'
-archivo = "#{BASE_DIR}/inventario.yml"
+archivo = "#{BASE_DIR}/Inventario Babaco - Hoja 1.csv"
 
 def snake_case(string)
   string.downcase.gsub(' ', '-')
 end
 
 def to_articulo(item)
-  { 
-    'titulo' => item['Nombre'], 
-    'descripcion' => item['Descripción'], 
-    'cantidad' => item['Cantidad'].empty? ? 1 : item['Cantidad'].to_i,
+  {
+    'titulo' => item['Nombre'],
+    'descripcion' => item['Descripción'],
+    'cantidad' => item['Cantidad'].nil? ? 1 : item['Cantidad'].to_i,
     'categoria' => item['Rubro'],
-    'foto1' => '',
-    'foto2' => '',
-    'foto3' => ''
+    'foto1' => item['Foto 1'] || '',
+    'foto2' => item['Foto 2'] || '',
+    'foto3' => item['Foto 3'] || ''
   }
 end
 
@@ -27,7 +28,7 @@ def generar_archivo(articulo)
   end
 end
 
-inventario = YAML.load_file archivo
+inventario = CSV.read(archivo, headers: true).map(&:to_h)
 
 articulos = 
   inventario
