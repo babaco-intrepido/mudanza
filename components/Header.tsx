e1283d88942745d6f21800d891c270cdec956ed0
Federico Aloi <federico.aloi@gmail.com>
2022-04-20 17:42:20 -0300

    Navegación a la home

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,15 @@
 import AppBar from '@mui/material/AppBar';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Link from './Link';
 
 const Header = () => {
   return (
     <AppBar position="relative">
       <Toolbar>
-        <CameraIcon sx={{ mr: 2 }} />
-        <Typography variant="h6" color="inherit" noWrap>
-          Album layout
-        </Typography>
+        <Link href="/" variant="h6" color="inherit" underline="none" noWrap>
+          ¡Vendemos todo!
+        </Link>
       </Toolbar>
     </AppBar>
   );
