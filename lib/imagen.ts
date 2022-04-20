2351e1ae6f924c028f4ae4549332a14f32783d58
Federico Aloi <federico.aloi@gmail.com>
2022-04-20 19:13:35 -0300

    Placeholder para artículos sin imagen

--- lib/imagen.ts
+++ lib/imagen.ts
@@ -1,8 +1,11 @@
 import type { Resize } from '@cld-apis/types';
 import { buildUrl } from 'cloudinary-build-url';
+import { isEmpty } from 'ramda';
+
+const placeholder = 'placeholder_oz5mpd.png';
 
 export function imagenConResolucion(nombre: string, resize: Resize) {
-  return buildUrl(nombre, {
+  return buildUrl(isEmpty(nombre) ? placeholder : nombre, {
     cloud: { cloudName: 'faloi' },
     transformations: { resize },
   });
