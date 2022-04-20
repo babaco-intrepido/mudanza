aee40ef160bcc08e2fbacabfdd6e8fc258114a5c
Federico Aloi <federico.aloi@gmail.com>
2022-04-20 12:53:01 -0300

    Colores primario y secundario

--- styles/theme/lightThemeOptions.ts
+++ styles/theme/lightThemeOptions.ts
@@ -3,6 +3,18 @@ import { ThemeOptions } from '@mui/material/styles';
 const lightThemeOptions: ThemeOptions = {
   palette: {
     mode: 'light',
+    primary: {
+      light: '#4c8c4a',
+      main: '#1b5e20',
+      dark: '#003300',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#b4a647',
+      main: '#827717',
+      dark: '#524c00',
+      contrastText: '#fff',
+    },
   },
 };
 
