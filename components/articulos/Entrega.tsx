66d79ceb0fdb2e2a2563db3cdc1607680cedfda6
Federico Aloi <federico.aloi@gmail.com>
2022-04-21 21:44:10 -0300

    Componente vacío si no hay entrega

--- components/articulos/Entrega.tsx
+++ components/articulos/Entrega.tsx
@@ -11,14 +11,14 @@ export interface EntregaProps {
 }
 
 export default function Entrega({ articulo, size }: EntregaProps) {
-  return (
-    articulo.entrega && (
-      <RectangularChip
-        label={`Disponible a partir de ${articulo.entrega}`}
-        variant="outlined"
-        color="warning"
-        size={size}
-      />
-    )
+  return articulo.entrega ? (
+    <RectangularChip
+      label={`Disponible a partir de ${articulo.entrega}`}
+      variant="outlined"
+      color="warning"
+      size={size}
+    />
+  ) : (
+    <></>
   );
 }
