a0597171f201041b298390aa37a0304496d47bda
Federico Aloi <federico.aloi@gmail.com>
2022-04-26 11:30:59 -0300

    Corregido image path en OG

--- components/OgTags.tsx
+++ components/OgTags.tsx
@@ -22,14 +22,14 @@ export default function OgTags({
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta name="twitter:title" content={title}></meta>
       <meta name="twitter:description" content={description}></meta>
-      <meta name="twitter:image" content={siteUrl + imagePath}></meta>
+      <meta name="twitter:image" content={imagePath}></meta>
       <meta property="og:locale" content="es_AR" />
       <meta property="og:site_name" content={site} />
       <meta property="og:title" content={title}></meta>
       <meta property="og:type" content="article"></meta>
       <meta property="og:url" content={`${siteUrl}/${path}`}></meta>
       <meta property="og:description" content={description}></meta>
-      <meta property="og:image" content={siteUrl + imagePath}></meta>
+      <meta property="og:image" content={imagePath}></meta>
     </Head>
   );
 }
