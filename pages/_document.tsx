c70c1235654bc8c7416702ec93daed406ef21fed
Federico Aloi <federico.aloi@gmail.com>
2022-04-26 11:07:42 -0300

    Título de la página

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,8 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+          <title>¡Nos mudamos!</title>
         </Head>
         <body>
           <Main />
