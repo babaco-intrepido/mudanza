7a9a6dbb5f8f38935129c76b88b0e877a01d8236
Federico Aloi <federico.aloi@gmail.com>
2024-09-01 23:40:42 -0300

    Landing completa

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import Carrousel from '../components/Carrousel';
 
 const LaMovi = () => {
   return (
-    <Stack>
+    <Stack spacing={1}>
       <Typography variant="h5" gutterBottom>
         Nuestra casa sobre ruedas
       </Typography>
@@ -24,9 +24,37 @@ const LaMovi = () => {
         Ideal para iniciar una vida sobre ruedas o para poder vacacionar con la
         comodidad de tener tu propio espacio itinerante.
       </Typography>
-      <Stack direction="row" justifyContent="center">
+      <Stack direction="row" justifyContent="center" pt={1}>
         <Button component={Link} variant="contained" href="/venta">
           ¡Quiero saber más!
+        </Button>
+      </Stack>
+    </Stack>
+  );
+};
+
+const LasCosas = () => {
+  return (
+    <Stack spacing={1}>
+      <Typography variant="h5" gutterBottom>
+        Y además, ¡nos mudamos!
+      </Typography>
+      <Typography>
+        Una vez que se venda La Movi, nuestro plan es seguir viaje por
+        Latinoamérica en un nuevo formato y no podemos cargar mucho en nuestras
+        mochilas.
+      </Typography>
+      <Typography>
+        Por este motivo, también queremos circular prácticamente todo lo que
+        teníamos en nuestra casita. Algunas cosas están a la venta y otras las
+        ofrecemos de regalo para quienes nos compren algo.
+      </Typography>
+      <Stack direction="row" spacing={2} justifyContent="center" pt={1}>
+        <Button component={Link} variant="contained" href="/venta">
+          A la venta
+        </Button>
+        <Button component={Link} variant="outlined" href="/regalo">
+          De regalo
         </Button>
       </Stack>
     </Stack>
@@ -47,14 +75,6 @@ const Index: NextPage = () => {
           Somos Andre, Fede y Saywa, una familia viajera que se agrandó en el
           camino y necesita vender su casa para poder seguir viajando.
         </Typography>
-        {/* <Stack direction="row" spacing={2} justifyContent="center">
-          <Button component={Link} variant="contained" href="/venta">
-            A la venta
-          </Button>
-          <Button component={Link} variant="outlined" href="/regalo">
-            Regalado
-          </Button>
-        </Stack> */}
         <Carrousel
           images={[laMovi.src, laMovi.src, laMovi.src]}
           maxWidth="100%"
@@ -62,6 +82,7 @@ const Index: NextPage = () => {
           height={900}
         />
         <LaMovi />
+        <LasCosas />
       </Stack>
     </>
   );
